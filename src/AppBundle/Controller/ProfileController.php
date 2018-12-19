83ec49c93babc2770e1c8b28c754fb2a2ab8eb2e
Giedrius Slegeris <giedrius@iname.com>
2018-12-19 16:37:36 +0000

    Profile addition page added with a simple form. Add profile page is set to submit the form to itself. Add profile link added to general profile page.

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -14,10 +14,34 @@ class ProfileController extends Controller
    */
   public function profile(Request $request)
   {
-      // replace this example code with whatever you need
       return $this->render('profile/profile.html.twig', array(
           'title' => 'Profile page',
-          'name' => 'Giedrius profile'
+          'name' => 'Giedrius profile',
+          'profiles' => [
+            1, 2, 3, 4, 5
+          ]
+      ));
+  }
+
+  /**
+   * @Route("/profile/add", name="add_profile")
+   */
+  public function profileAdd(Request $request)
+  {
+      return $this->render('profile/profile_add.html.twig', array(
+          'title' => 'Add new profile',
+          'call' => $request->getMethod()
+      ));
+  }
+
+  /**
+   * @Route("/profile/{user_id}", name="profileID")
+   */
+  public function profileID(Request $request, int $user_id)
+  {
+      return $this->render('profile/profile_id.html.twig', array(
+          'title' => 'Profile with ID',
+          'profile_id' => $user_id
       ));
   }
 
